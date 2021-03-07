c9d9395b43dcf11394cea96fa48acc7853ec51c4
Fatmats <melikeats0561@gmail.com>
2021-03-07 18:17:28 +0300

    ınıt

---
+++ 1-Vuex & localstroge/src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about" >
+    <h1>{{image}}</h1>
+  </div>
+</template>
+<script>
+import {mapState} from 'vuex'
+export default {
+  computed:{
+    ...mapState([
+      'image'
+    ])
+  },
+ 
+}
+</script>
